330d6775219ace02ca4e4343d8436f99b1e64818
kiranmahendragandhi <55480634+kiranmahendragandhi@users.noreply.github.com>
2026-06-14 12:36:19 +0400

    Update bonus_calc.py

--- bonus_calc.py
+++ bonus_calc.py
@@ -1,16 +1,20 @@
 # bonus_calc.py
+import os
 
-# 1. Define the data
+# 1. Read the environment choice from Jenkins
+# If Jenkins doesn't send anything, it defaults to 'Local-Test'
+env_choice = os.environ.get('ENVIRONMENT', 'Local-Test')
+
+print(f"=== DEPLOYMENT ENVIRONMENT: {env_choice.upper()} ===")
+print("=== STARTING BONUS CALCULATION ===")
+
 employees = {
     "Alice": 5000,
     "Bob": 6200,
     "Charlie": 4500
 }
-bonus_rate = 0.10  # 10% bonus
+bonus_rate = 0.10 
 
-print("=== STARTING BONUS CALCULATION ===")
-
-# 2. Process the data and print results
 for name, salary in employees.items():
     bonus = salary * bonus_rate
     print(f"Employee: {name} | Salary: ${salary} | Bonus: ${bonus:.2f}")
